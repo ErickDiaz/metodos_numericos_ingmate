02f3771466faccaf06b8448dd797cbf4a75c3aad
Erick Díaz Saborio <ewdiazsa@gmail.com>
2020-12-21 03:48:36 -0600

    comentarios en el codigo

--- laboratorio_1/heun_vec.m
+++ laboratorio_1/heun_vec.m
@@ -1,11 +1,11 @@
 function [x,y, y_filter] = heun_vec(f, a, b, N, y0, x_filter)
-h= (b-a)/N;
-x=a:h:b;
-x=x(:);
-y=zeros(N+1, length(y0));
-y(1,:)=y0;
+h = (b-a)/N;
+x = a:h:b;
+x = x(:);
+y = zeros(N+1, length(y0));
+y(1,:) = y0;
 
-y_filter=zeros(length(x_filter), length(y0));
+y_filter = zeros(length(x_filter), length(y0));
 filter_index = 1;
 
 for k=1:N
@@ -13,6 +13,7 @@ for k=1:N
     k2 = h * feval(f,x(k+1),y(k,:)+k1)';
     y(k+1,:) = y(k,:) + (k1+k2)/2;
     
+    % Filtrando valores de y con un vector( x_filter)
     if ismember(x(k), x_filter)
         y_filter(filter_index, :) = y(k,:);
         filter_index = filter_index + 1;
